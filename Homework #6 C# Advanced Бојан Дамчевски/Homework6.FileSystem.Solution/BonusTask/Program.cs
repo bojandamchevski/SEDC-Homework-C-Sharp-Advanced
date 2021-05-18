733224e990ddfe52ff98b34ae09cddda7644704a
bojandamchevski <bojandamcevski@gmail.com>
2021-05-19 00:26:20 +0200

    Homework6

---
+++ Homework #6 C# Advanced Бојан Дамчевски/Homework6.FileSystem.Solution/BonusTask/Program.cs
@@ -0,0 +1,73 @@
+﻿using BonusTask.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BonusTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John", "Smith", 43);
+            Person person2 = new Person("Julliana", "Craine", 29);
+            Person person3 = new Person("Joe", "Blake", 32);
+
+            List<Person> people = new List<Person>()
+            {
+                person1,
+                person2,
+                person3
+            };
+
+            string folderPath = @"..\..\..\People";
+            string filePath = folderPath + @"\people.txt";
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                string allPeople = "";
+                for (int i = 0; i < people.Count; i++)
+                {
+                    if (i == people.Count - 1)
+                    {
+                        allPeople += $"{people[i].FirstName} {people[i].LastName} {people[i].Age}";
+                    }
+                    else
+                    {
+                        allPeople += $"{people[i].FirstName} {people[i].LastName} {people[i].Age}\n";
+                    }
+                }
+                streamWriter.WriteLine(allPeople);
+            }
+
+            List<Person> newPeople = new List<Person>();
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                for (int i = 0; i < people.Count; i++)
+                {
+                    string person = streamReader.ReadLine();
+                    string[] info = person.Split(" ");
+                    Person newPerson = new Person(info[0], info[1], Convert.ToInt32(info[2]));
+                    newPeople.Add(newPerson);
+                }
+            }
+
+            foreach (Person person in newPeople)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName} {person.Age}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework #6 C# Advanced Бојан Дамчевски/Homework6.FileSystem.Solution/Homework6.FileSystem.Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Homework6.FileSystem.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Exercise";
+            string filePath = folderPath + @"\calculations.txt";
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.WriteLine(ActionTaker());
+            }
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                string readResult = streamReader.ReadToEnd();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(readResult);
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+
+            Console.ReadKey();
+        }
+        public static string Calculate(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            string resultString = $"The equation is: {number1} + {number2}.\nAnd the result is: {sum}\n\n\n";
+            Console.ResetColor();
+            return resultString;
+        }
+        public static string ActionTaker()
+        {
+            string result = "";
+            int i = 1;
+            while (i <= 3)
+            {
+                Console.WriteLine("Enter the first number:\n");
+                bool validationForTheFirstNumber = int.TryParse(Console.ReadLine(), out int num1);
+                Console.WriteLine("Enter the second number:\n");
+                bool validationForTheSecondNumber = int.TryParse(Console.ReadLine(), out int num2);
+
+                if (validationForTheFirstNumber && validationForTheSecondNumber)
+                {
+                    result += Calculate(num1, num2);
+                }
+
+                i++;
+            }
+            return result;
+        }
+    }
+}
